6ef1069db9571f6d848dd0d22053931d48e9f6e6
Ihor-Usenko <kievus70@gmail.com>
2024-04-22 02:36:14 +0300

    correction

--- home_work3-2.py
+++ home_work3-2.py
@@ -1,17 +1,17 @@
-from random import randint
+from random import sample
 
 
 def get_numbers_ticket(min, max, quantity) -> list:
-    set_number = set()
+    set_number = []
     try:
-        if min >= 1 and max <= 1000:
-            while len(set_number) < quantity:
-                set_number.add(randint(min, max))
+        if min >= 1 and max <= 1000 and max - min >= quantity:
+            set_number = sorted(sample(range(min, max + 1), quantity))
         else:
-            return list(set_number)
-        return sorted(list(set_number))
+            return set_number
+        return set_number
+
     except TypeError as m:
         print(m)
 
 
-print(get_numbers_ticket(1, 49, 6))
+print(get_numbers_ticket(1, 10, 9))
